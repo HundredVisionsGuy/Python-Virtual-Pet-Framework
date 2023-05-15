e8507466dffa3dc4fcaac920ba67e6c3257795b5
hundredvisionsguy <cwinikka@gmail.com>
2023-05-15 10:49:36 -0700

    Adding a main file and todo list

---
+++ virtual_pet_framework/main.py
@@ -0,0 +1,24 @@
+"""
+This is the main virtual pet program. 
+TODO: create an interaction loop that provides ways to interact with your pet.
+Examples:
+* create a pet
+* save your pet
+* view all pets
+* bring out your pet
+* play with your pet
+* feed your pet
+* train your pet
+"""
+
+from pet import Pet
+import utilities
+
+
+def main():
+    print("Hello. Welcome to my program")
+    # TODO: review the examples from above and work them into your program.
+
+
+if __name__ == "__main__":
+    main()
