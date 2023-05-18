ee030ab00bff6f62775cef94ac31b9e08c53e82f
hundredvisionsguy <winikkc@hsd.k12.or.us>
2023-05-18 15:03:52 -0700

    adding user choices

--- virtual_pet_framework/main.py
+++ virtual_pet_framework/main.py
@@ -12,9 +12,10 @@ Examples:
 """
 
 import utilities
-import pet
+from pet import Pet
 
 def main():
+    active_pets = []
     # Create a menu for pet playground
     new_menu = """
     Here is your list of options:
@@ -28,6 +29,23 @@ def main():
     choice = ""
     while choice != "4":
         choice = utilities.get_menu_choice(new_menu, options)
+        if choice == "1":
+            pet = create_new_pet()
+            active_pets.append(pet)
+        elif choice == "2":
+            if not active_pets:
+                print("\nYou have no pets to play with.")
+        elif choice == "3":
+            if active_pets:
+                for pet in active_pets:
+                    pet.store_pet_data()
+
+def create_new_pet():
+    """ creates and returns a new pet object"""
+    name = input("Select a name for your pet: ")
+    breed = input("Select a pet breed: ")
+    pet = Pet(name, breed)
+    return pet
 
     print("\nThanks for playing.")
 
